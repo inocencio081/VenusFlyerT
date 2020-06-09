a2e69af923ad218743b27f20613cea7fc6d8bcc6
Inocêncio <inocencio080carlos@gmail.com>
2020-06-08 21:49:40 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Venus Flyer</title>
+<link rel="stylesheet" type="text/css" href="css/new.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body> 
+	<div class="all">
+	
+<!-- parte superior localizado o logotipo -->
+	<section id="home" class="superior">
+		<div class="row">
+			<div class="col-md-6 Logotipo">
+				<img class="img-fluid" src="imagens/Logotipo/venusLogo.png">
+			</div>	
+				<div class="col-md-3">
+					<div class="endereco">
+						<h6><i class="fa fa-map-marker f-color"></i> Luanda, Angola </h6>
+						<h6><i class="fa fa-phone f-color"></i>  (+244) 925-512-252</h6>
+						<h6><i class="fa fa-envelope f-color"></i> inocarlos90@gmail.com</h6>
+					</div>
+				</div>
+				<div class="col-md-3 superior-right">
+						<img class="img-fluid text-left" src="imagens/Logotipo/qr-code.jpg">
+				 </div>
+		</div>
+	</section>
+<!-- Fim da parte superior localizado o logotipo -->
+<!-- Imagem central -->
+<section class="imagem-central">
+	<div class="imagem-focus">
+			<img class="img-fluid" src="imagens/empresario.jpg">
+		<div class="opacity">
+				<div class="col-md-6 text-left contexto-central">
+					<h1> Ajudamos Marcas </h1>
+					<h1><span class="f-dash">Con</span>ectando-se com <span class="f-color">pessoas</span></h1>
+				</div>
+		</div>
+	</div>
+</section>
+<!-- fim da Imagem central -->
+<!-- inicio da section do meio corpo -->
+<section class="contexto-corpo">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 text-right">
+				<h6 class="corpo-h6"> <span class="font-weight-bold">Trabalho</span> <br>
+				 <span style="color: #ccc;">criativo em estúdio</span></h6>
+				<h3 class="corpo-h5"> Os nossos serviços garantem um fluxo de trabalho íntegro, assim como um escopo bem definido  para evitar sustos e pegadinhas no decorrer do projeto. Ferramentas e metodologias estão disponíveis para serem utilizadas de acordo com a natureza de cada trabalho. Uma equipe bem integrada com seus recursos é determinante para que o cronograma seja respeitado e até mesmo superado. <br><br>
+
+				 Ainda que estratégias de marketing digital possam ser implementadas nos casos mais adversos, um projeto bem planejado e executado é o diferencial que confere flexibilidade e agilidade na hora de otimizar o produto ou serviço para que este performe sempre da melhor maneira para o usuário.</h3>
+			</div>
+				<div class="col-md-4">
+					<div class="f-corpo-lateral">
+						<h6 class="font-weight-bold">Olá! <br>Nós Criamos <br> Projeto de Sucesso <br> e Produção Digital</h6>
+						<h6 class="text-justify">Sob a nossa perspectiva do desenvolvimento de Projetos Digitais com sucesso, o ato de medir se transpõe nas primeiras reuniões, em que devem ser realizadas as definições de escopo com cliente e equipe. </h6>
+					</div>
+				</div>
+		</div>
+
+	</div>
+</section>
+<!--fim da section do meio corpo -->
+
+<!--inicio da section de seviços -->
+<section id="servico" class="servicos breadcrumb">
+		<div class="col-md-4 servico-no-dotted">
+			<a href=""><i class="fa fa-pen servico-fa"></i> 
+			<h6 class="f-corpo-h6"> Creative <br>
+			<span class="font-weight-bold">Bussiness Job</span></h6></a>
+			 <h6>Os nossos serviços garantem um fluxo de trabalho íntegro, assim como um escopo bem definido  para evitar sustos e pegadinhas no decorrer do projeto. Ferramentas e metodologias estão disponíveis para serem utilizadas de acordo com a natureza de cada trabalho.</h6>
+		</div>
+			<div class="col-md-4  servico-dotted">
+				<a href=""><i class="fa fa-tasks servico-fa"></i>
+				<h6 class="f-corpo-h6"> Creative<br>
+				<span class="font-weight-bold">Design Solutions</span></h6></a>
+				 <h6>Os nossos serviços garantem um fluxo de trabalho íntegro, assim como um escopo bem definido  para evitar sustos e pegadinhas no decorrer do projeto. Ferramentas e metodologias estão disponíveis para serem utilizadas de acordo com a natureza de cada trabalho.</h6>
+			</div>
+				<div class="col-md-4  servico-dotted">
+				<a href="">	<i class="fab fa-telegram-plane servico-fa"></i> 
+					<h6 class="f-corpo-h6">Creative<br>
+					<span class="font-weight-bold">Transparrent</span></h6></a>
+					 <h6>Os nossos serviços garantem um fluxo de trabalho íntegro, assim como um escopo bem definido  para evitar sustos e pegadinhas no decorrer do projeto. Ferramentas e metodologias estão disponíveis para serem utilizadas de acordo com a natureza de cada trabalho.</h6> 
+				</div> 	
+</section>
+<!--fim da section de seviços -->
+
+<!--inicio da section sobre nos -->
+<section id="About" class="container sobre-nos">
+	<div class="corpo-sobre">
+		<div class="row">
+			<div class="col-md-12 px-4"><h1>Porque<span class="f-color"> Escolher-nos?</span></h1></div>
+		</div>
+		<div class="row"> 
+			<div class="col-md-5 corpo-sobre-pontos text-center">
+				<i class="fas fa-check-circle f-color"></i> Lorem Ipsum is simply dummy text the printing<br>
+				<i class="fas fa-check-circle f-color"></i> Lorem Ipsum is simply dummy text the printing<br>
+				<i class="fas fa-check-circle f-color"></i> Lorem Ipsum is simply dummy text the printing<br>
+				<i class="fas fa-check-circle f-color"></i> Lorem Ipsum is simply dummy text the printing<br>
+				<br>
+				<form class="breadcrumb">
+				
+					<div class=" col-md-12 form-group">	
+				    <input type="email" class="form-control" placeholder="Nome">
+				  </div>
+					<div class="col-md-12 form-group">	
+				    <input type="email" class="form-control" placeholder="email">
+				  </div>
+					<button class="btn btn-warning">Subscrever</button>
+				</form>
+			</div>
+				<div class="col-md-2"></div>
+					<div class="col-md-5 sobre-descontos">
+						<img class="img-fluid" src="imagens/descontoPrim.jpg">
+							<h6><a href="">Transparrent</a></h6>
+							<h5><a href="">Transparrent</a></h5>
+							<h4><a href="">Transparrent</a></h4>
+							<button class="btn btn-warning"> Saber + sobre Descontos</button>
+					</div>
+		</div>
+	</div>
+
+</section>
+<!-- fim da section sobre nos -->
+
+<!-- inicio da section footer -->
+<section id="Contact" class="footer text-center">
+	<footer class="footer-dow">
+		<div class="container text-center">
+			<h3 class="font-weight-bold mt-5"></b>Vênus, Redes Sociais</h3> <br>
+			  <div class="redes-icones">
+				  	<a href=""><i class="fab fa-facebook footer-f"></i></a>
+				  	<a href=""><i class="fab fa-whatsapp footer-f"></i></a>
+				  	<a href=""><i class="fab fa-twitter footer-f"></i></a>
+				  	<a href=""><i class="fa fa-envelope footer-f"></i></a>
+			  </div>
+		</div>
+
+		<div class="container row">
+			<div class="col-md-3 footer-box">
+				<a href="#"><img class="img-fluid" src="imagens/Logotipo/logotipo.png"></a>
+					<div class="menu">
+						<h6><a href="#superior"><i class="fa fa-home footer-fa"></i> Início</a></h6>
+						<h6><a href="#servicos"><i class="fa fa-briefcase footer-fa"></i> Serviços</a></h6>
+						<h6><a href="#sobre-nos"><i class="fa fa-map-marker footer-fa"></i> Sobre nós</a></h6>
+						<h6><a href="#sobre-nos"><i class="fa fa-award footer-fa"></i> Porque escolher-nos </a></h6>
+					</div>
+			</div>
+			<div class="col-md-3 footer-box">
+				<h3 class="font-weight-bold"></b>Projectos</h3> <br>
+					<h6><a href="#"><i class="fa fa-arrow-right footer-fa"></i> VenusFlyerT</a></h6>
+					<h6><a href="#"><i class="fa fa-arrow-right footer-fa"></i> Bootstrap 4 Venus </a></h6>
+					<h6><a href="#"><i class="fa fa-arrow-right footer-fa"></i> WebsiteBusiness </a></h6>
+					<h6><a href="#"><i class="fa fa-arrow-right footer-fa"></i> Website Design</a></h6>
+			</div>
+				<div class="col-md-3 footer-box">
+					<h3 class="font-weight-bold"></b>Apoio, Suporte</h3> <br>
+						<h6><a href="#"><i class="fa fa-toolbox footer-fa"></i> Solução de Problemas</a></h6>
+						<h6><a href="#"><i class="fa fa-headset footer-fa"></i> Perguntas</a></h6>
+						<h6><a href="#"><i class="fa fa-bug footer-fa"></i> Reportar um Erro</a></h6>
+						<h6><a href="#"><i class="fa fa-question footer-fa"></i> Obtenha Ajuda</a></h6>
+				</div>
+			<div id="contactos" class="col-md-3 footer-box">
+					<h3><b> Companhia</b></h3> <br>
+					<h6><a href="#"><i class="fa fa-blog footer-fa"></i> Blog </a></h6>
+					<h6><a href="#"><i class="fa fa-universal-access footer-fa"></i>  Status </a></h6>
+					<h6><a href="#"><i class="fa fa-envelope footer-fa"></i> Entre em Contacto</a></h6>
+					<h6><a href="#"><i class="fa fa-globe-africa footer-fa"></i> @venus</a></h6>
+			</div>
+		</div>
+	
+	<!-- inicio do rodape -->
+	<div class="breadcrumb"> 
+		<div class="col-md-12 text-center ">
+			<h3>2013 - 2020 | © Vênus, <a href="">Termos e Privacidades</a></h3>
+		</div>
+			
+	</div>
+	</footer>
+</section>
+
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/all.js"></script>
+</body>
+</html>
